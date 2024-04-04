b1b1da7295b22427cd1101de1477734e24b05ed4
zahrabateninia <zahrabatenin@gmail.com>
2024-04-04 11:55:45 +0330

    Create a constructor function for that initialize bucket and size of hash table
    Set the hash table's size to a number that is power of two (16) because it makes the hashing table more efficient

---
+++ hashMap.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+class HashMap{
+    constructor(){
+        // Hold the key-value pairs in a bucket
+        this.bucket = [];
+        // Set the initial size for hash table
+        this.size = 16;
+    }    
+}
