d30de61d3c8814d99af9ab38dd739181a92a0ed1
zahrabateninia <zahrabatenin@gmail.com>
2024-04-04 12:29:27 +0330

    Add hash method
    Start with an empty tag( hashCode). For each letter in the key:   hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % size
    
    We want to make sure that our tag fits into the size of hash table. So, we use the modulo operator (%) to find the remainder when dividing the tag by
    the size of the box. This ensures that our tag stays within the range of possible indices in our hash table.

--- hashMap.js
+++ hashMap.js
@@ -6,4 +6,18 @@ class HashMap{
         // Set the initial size for hash table
         this.size = 16;
     }    
+    // hash function : it takes a key and produces a hash code
+    hash(key, size) {
+        let hashCode = 0;
+        const primeNumber = 31;
+        
+        for (let i = 0; i < key.length; i++) {
+            // Apply modulo operator on each iteration to prevent overflow
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % size;
+        }
+        
+        return hashCode;
+}
+
+     
 }
